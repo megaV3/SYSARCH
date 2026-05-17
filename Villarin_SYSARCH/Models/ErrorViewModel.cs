538cd3549b1c6fbe0aca41f40b6d69c53d03e2f8
megaV3 <132799339+megaV3@users.noreply.github.com>
2026-05-17 16:45:00 +0800

    Moving files

--- Villarin_SYSARCH/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Villarin_SYSARCH.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
